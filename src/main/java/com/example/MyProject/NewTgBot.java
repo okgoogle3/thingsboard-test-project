1914514e98edbcf3d67a356b5881544b817d1294
okgoogle3 <halalah.han@gmail.com>
2023-02-16 21:54:50 +0200

    Some fixes of tg bot

--- src/main/java/com/example/MyProject/NewTgBot.java
+++ src/main/java/com/example/MyProject/NewTgBot.java
@@ -7,8 +7,7 @@ import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import org.springframework.aop.scope.ScopedProxyUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class NewTgBot {
@@ -18,9 +17,13 @@ public class NewTgBot {
         GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
         //List<Update> updates = updatesResponse.updates().stream().filter(update -> update.message().text().equals("/start")).toList();
         List<Update> updates = updatesResponse.updates();
-        List<Message> messages = new ArrayList<>();
-        updates.forEach(update -> messages.add(update.message()));
+        Set<Long> chat_id = new HashSet<>();
+        updates.removeIf(element -> element.message() == null);
+        updates.forEach(update -> chat_id.add(update.message().chat().id()));
+
+        chat_id.forEach(System.out::println);
         //messages.forEach(message -> System.out.println(message.chat().id()));
-        messages.forEach(System.out::println);
+        //messages.forEach(message -> System.out.println(message));
+        //messages.forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/example/MyProject/aspect/AspectBot.java
@@ -0,0 +1,67 @@
+package com.example.MyProject.aspect;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import jakarta.annotation.PostConstruct;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@Aspect
+@Component
+public class AspectBot {
+    TelegramBot bot = new TelegramBot("6116276576:AAEurzwvN0eUyps8aGomnw8Xwn6Tqwp78Sk");
+    Set<Long> chat_id = new HashSet<>();
+
+    @PostConstruct
+    public void initialization() {
+        /*GetUpdates getUpdates = new GetUpdates().limit(100).offset(0).timeout(0);
+        GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
+        //List<Update> updates = updatesResponse.updates().stream().filter(update -> update.message().text().equals("/start")).toList();
+        List<Update> updates = updatesResponse.updates();*/
+
+        bot.setUpdatesListener(updates -> {
+            updates.removeIf(element -> element.message() == null);
+            Long id;
+            for (Update update : updates) {
+                id = update.message().chat().id();
+                if(update.message().text().equals("/subscribe")) {
+                    if (chat_id.contains(id)) bot.execute(new SendMessage(id, "You are a subscriber."));
+                    else {
+                        chat_id.add(id);
+                        bot.execute(new SendMessage(id, "You have subscribed on notifications!"));
+                    }
+                }
+                if(update.message().text().equals("/unsubscribe")) {
+                    if (chat_id.contains(id)) {
+                        chat_id.remove(id);
+                        bot.execute(new SendMessage(id, "You have unsubscribed from notifications."));
+                    }
+                    else bot.execute(new SendMessage(id, "You are not a subscriber."));
+                }
+            }
+            //updates.forEach(update -> chat_id.add(update.message().chat().id()));
+            //System.out.println(updates);
+            //System.out.println(chat_id);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
+
+    @Pointcut("execution(public * com.example.MyProject.service.DeviceService.*(..)) || execution(public * com.example.MyProject.service.AssetService.*(..))")
+    public void callAtMyServicePublic() { }
+
+    @After("callAtMyServicePublic()")
+    public void afterCallAt() {
+        chat_id.parallelStream().forEach(id -> bot.execute(new SendMessage(id, "Aspect is working rn")));
+    }
+}
