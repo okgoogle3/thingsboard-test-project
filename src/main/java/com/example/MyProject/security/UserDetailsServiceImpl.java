5edb450d91f3d1561ed2072575320093c02ad220
okgoogle3 <halalah.han@gmail.com>
2023-02-22 18:16:28 +0200

    Some fixes (not working version)

---
+++ src/main/java/com/example/MyProject/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.MyProject.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found with username: " + email);
+        }
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword().toLowerCase(), enabled, accountNonExpired,
+                credentialsNonExpired, accountNonLocked, getAuthorities(user.getRoles()));
+    }
+
+}
