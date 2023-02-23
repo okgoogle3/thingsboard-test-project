80f5ca07bd178ef34efada252e37b4e07739c1ec
okgoogle3 <halalah.han@gmail.com>
2023-02-23 12:05:38 +0200

    Some fixes

--- src/main/java/com/example/MyProject/security/SecurityConfig.java
+++ src/main/java/com/example/MyProject/security/SecurityConfig.java
@@ -14,13 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.sql.DataSource;
-
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
     UserDetailsServiceImpl userDetailsService;
+    @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
