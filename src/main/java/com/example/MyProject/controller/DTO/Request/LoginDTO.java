46296edc3c5afb2cb51c3489dd829e6f44b49468
okgoogle3 <halalah.han@gmail.com>
2023-02-24 21:40:43 +0200

    Some fixes

--- src/main/java/com/example/MyProject/controller/DTO/Request/LoginDTO.java
+++ src/main/java/com/example/MyProject/controller/DTO/Request/LoginDTO.java
@@ -1,27 +1,12 @@
 package com.example.MyProject.controller.DTO.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class LoginDTO {
     @NotBlank
     private String username;
-
     @NotBlank
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
